418cc8dda5cc0a42c11b4147944c1a1195807cd1
= <=>
2017-03-30 19:57:20 -0500

    documentando los modelos y creando las vistas basadas en clase

--- ProyectoDHS/apps/acudiente/models.py
+++ ProyectoDHS/apps/acudiente/models.py
@@ -13,12 +13,17 @@ from django.db import models
 
 
 class Acudiente(models.Model):
-    
+    """
+    Clase encargada de representar el acudiente del perro 
+    """
     nombre = models.TextField()
     direccion = models.TextField()
     telefono = models.TextField()
     email = models.TextField(blank=True, null=True)
     alias = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.nombre
 
     class Meta:
         managed = False
@@ -26,6 +31,9 @@ class Acudiente(models.Model):
 
 
 class Alimentacion(models.Model):
+    """
+    Clase encargada de representar la alimentación del perro 
+    """
     unidad_medida = models.ForeignKey('UnidadMedida', models.DO_NOTHING, db_column='unidad_medida')
     fecha = models.DateField()
 
@@ -35,6 +43,9 @@ class Alimentacion(models.Model):
 
 
 class Color(models.Model):
+    """
+    Clase encargada de representar el color del perro 
+    """
     nombre = models.TextField()
 
     class Meta:
@@ -43,6 +54,9 @@ class Color(models.Model):
 
 
 class Dia(models.Model):
+    """
+    Clase encargada de representar el dia de alimentación del perro 
+    """
     nombre = models.TextField()
 
     class Meta:
@@ -51,6 +65,9 @@ class Dia(models.Model):
 
 
 class Dieta(models.Model):
+    """
+    Clase encargada de representar la dieta del perro 
+    """
     tipo_dieta = models.ForeignKey('TipoDieta', models.DO_NOTHING, db_column='tipo_dieta')
 
     class Meta:
@@ -59,6 +76,9 @@ class Dieta(models.Model):
 
 
 class Ejercicio(models.Model):
+    """
+    Clase encargada de representar los ejercicios para adiestramiento del perro 
+    """
     nombre = models.TextField()
     descripcion = models.TextField()
 
@@ -68,6 +88,9 @@ class Ejercicio(models.Model):
 
 
 class EjercicioIntermedio(models.Model):
+    """
+    Clase encargada de representar los ejercicios asociados a un nivel de entrenamiento 
+    """
     id_ejercicio = models.ForeignKey(Ejercicio, models.DO_NOTHING, db_column='id_ejercicio')
     id_nivel_entrenamiento = models.ForeignKey('NivelEntrenamiento', models.DO_NOTHING, db_column='id_nivel_entrenamiento')
     tiempo_entrenamiento = models.IntegerField()
@@ -78,6 +101,9 @@ class EjercicioIntermedio(models.Model):
 
 
 class EstadoPerruno(models.Model):
+    """
+    Clase encargada de representar el estado del perro (reservado, apto para rescate, apto para busqueda) 
+    """
     nombre = models.TextField()
 
     class Meta:
@@ -86,6 +112,9 @@ class EstadoPerruno(models.Model):
 
 
 class HorarioDieta(models.Model):
+    """
+    Clase encargada de representar el horario de la dieta de un perro 
+    """
     id_dieta = models.ForeignKey(Dieta, models.DO_NOTHING, db_column='id_dieta', blank=True, null=True)
     id_alimentacion = models.ForeignKey(Alimentacion, models.DO_NOTHING, db_column='id_alimentacion')
     id_dia = models.ForeignKey(Dia, models.DO_NOTHING, db_column='id_dia')
@@ -97,6 +126,9 @@ class HorarioDieta(models.Model):
 
 
 class Madre(models.Model):
+    """
+    Clase encargada de representar la madre de un perro 
+    """
     id_raza = models.ForeignKey('Raza', models.DO_NOTHING, db_column='id_raza')
     id_color = models.ForeignKey(Color, models.DO_NOTHING, db_column='id_color')
     nombre = models.TextField()
@@ -108,6 +140,9 @@ class Madre(models.Model):
 
 
 class NivelEntrenamiento(models.Model):
+    """
+    Clase encargada de representar el nivel de entrenamiento de un perro 
+    """
     nombre = models.TextField()
 
     class Meta:
@@ -116,6 +151,9 @@ class NivelEntrenamiento(models.Model):
 
 
 class Padre(models.Model):
+    """
+    Clase encargada de representar el padre del perro 
+    """
     id_raza = models.ForeignKey('Raza', models.DO_NOTHING, db_column='id_raza')
     id_color = models.ForeignKey(Color, models.DO_NOTHING, db_column='id_color')
     nombre = models.TextField()
@@ -127,6 +165,9 @@ class Padre(models.Model):
 
 
 class ProductoAseo(models.Model):
+    """
+    Clase encargada de representar el producto de aseo que se le aplica a un perro 
+    """
     nombre = models.TextField()
 
     class Meta:
@@ -134,6 +175,9 @@ class ProductoAseo(models.Model):
         db_table = 'Producto_aseo'
 
 class Suplemento(models.Model):
+    """
+    Clase encargada de representar el suplemento que se le da a un perro 
+    """
     nombre = models.TextField()
     marca = models.IntegerField()
 
@@ -143,6 +187,9 @@ class Suplemento(models.Model):
 
 
 class Perro(models.Model):
+    """
+    Clase encargada de representar un perro 
+    """
     nombre = models.TextField(blank=True, null=True)
     fecha_nacimiento = models.DateField(blank=True, null=True)
     edad = models.IntegerField(blank=True, null=True)
@@ -172,6 +219,9 @@ class Perro(models.Model):
 
 
 class Raza(models.Model):
+    """
+    Clase encargada de representar la raza del perro 
+    """
     nombre = models.TextField()
 
     class Meta:
@@ -180,6 +230,9 @@ class Raza(models.Model):
 
 
 class Reserva(models.Model):
+    """
+    Clase encargada de representar la reserva de un perro 
+    """
     tipo_reserva = models.ForeignKey('TipoReserva', models.DO_NOTHING, db_column='tipo_reserva')
     fecha = models.DateField()
 
@@ -189,6 +242,9 @@ class Reserva(models.Model):
 
 
 class ReservaIntermedia(models.Model):
+    """
+    Clase encargada de representar la reserva la cual varias perros pueden estar asociados
+    """
     id_perro = models.ForeignKey(Perro, models.DO_NOTHING, db_column='id_perro', blank=True, null=True)
     id_reserva = models.ForeignKey(Reserva, models.DO_NOTHING, db_column='id_reserva', blank=True, null=True)
     precio_aproximado = models.IntegerField(blank=True, null=True)
@@ -199,6 +255,9 @@ class ReservaIntermedia(models.Model):
 
 
 class Tamanio(models.Model):
+    """
+    Clase encargada de representar el tamaño del perro 
+    """
     nombre = models.TextField()
 
     class Meta:
@@ -207,6 +266,9 @@ class Tamanio(models.Model):
 
 
 class TipoDieta(models.Model):
+    """
+    Clase encargada de representar el tipo de dieta del perro 
+    """
     nombre = models.TextField()
 
     class Meta:
@@ -215,6 +277,9 @@ class TipoDieta(models.Model):
 
 
 class TipoReserva(models.Model):
+    """
+    Clase encargada de representar el tipo de reserva al cual un perro está asociado. (adiestramiento, crianza, guarderia) 
+    """
     nombre = models.TextField(blank=True, null=True)
 
     class Meta:
@@ -223,6 +288,9 @@ class TipoReserva(models.Model):
 
 
 class TipoServicio(models.Model):
+    """
+    Clase encargada de representar el tipo de servicio al cual está adscrito un perro 
+    """
     nombre = models.TextField()
 
     class Meta:
@@ -231,6 +299,9 @@ class TipoServicio(models.Model):
 
 
 class TipoServicioPerro(models.Model):
+    """
+    Clase encargada de representar la asociación entre los perros con los servicios ofrecidos por el centro (adiestramiento, crianza, guarderia)
+    """
     fecha_entrada = models.DateField(blank=True, null=True)
     tiempo_estadia = models.DateField(blank=True, null=True)
     fecha_salida = models.DateField(blank=True, null=True)
@@ -244,6 +315,9 @@ class TipoServicioPerro(models.Model):
 
 
 class UnidadMedida(models.Model):
+    """
+    Clase encargada de representar la unidad de medida de la alimentación del perro 
+    """
     nombre = models.TextField()
 
     class Meta:
@@ -252,6 +326,9 @@ class UnidadMedida(models.Model):
 
 
 class Vacuna(models.Model):
+    """
+    Clase encargada de representar la vacuna del perro 
+    """
     nombre = models.TextField()
 
     class Meta:
@@ -260,6 +337,9 @@ class Vacuna(models.Model):
 
 
 class VacunaPerro(models.Model):
+    """
+    Clase encargada de representar la asociación entre vacuna y perro 
+    """
     id_vacuna = models.ForeignKey(Vacuna, models.DO_NOTHING, db_column='id_vacuna', blank=True, null=True)
     id_perro = models.ForeignKey(Perro, models.DO_NOTHING, db_column='id_perro', blank=True, null=True)
     fecha = models.DateField(blank=True, null=True)
@@ -271,6 +351,9 @@ class VacunaPerro(models.Model):
 
 
 class Veterinario(models.Model):
+    """
+    Clase encargada de representar el veterinario del perro 
+    """
     nombre = models.TextField()
     telefono = models.TextField()
     direccion = models.TextField()
--- ProyectoDHS/apps/acudiente/views.py
+++ ProyectoDHS/apps/acudiente/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
-
+from apps.acudiente.models import Acudiente
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView
 # Create your views here.
+
+class RegistrarAcudiente(CreateView):
+    """
+    Clase encargada de registrar el acudiente en la base de datos
+    """
+    #se ingresa qué modelo se utilizará para el registro
+    model = Acudiente
+    #se indica que formulario va a dar soporte a la acción de registro
+    #form_class = 
+    #se indica que template va a gestionar el registro
+    #template_name =
+    #se indica cual será la url de finalización
+    #success_url=reverse_lazy(namespace)
+
+class ActualizarInformacionAcudiente(UpdateView):
+    """
+    Clase encargada de actualizar la información del acudiente en la base de datos
+    """
+    #se ingresa qué modelo se utilizará para la actualización de la información
+    model = Acudiente
+    #se indica que formulario va a dar soporte a la acción de actualización de la información
+    #form_class = 
+    #se indica que template va a gestionar el registro
+    #template_name =
+    #se indica cual será la url de finalización
+    #success_url=reverse_lazy(namespace)
+
+class EliminarAcudiente(DeleteView):
+    """
+    Clase encargada de eliminar el acudiente de la base de datos
+    """
+    #se ingresa qué modelo se utilizará para el borrado de acudiente
+    model = Acudiente
+    #se indica que template va a gestionar el borrado
+    #template_name =
+    #se indica cual será la url de finalización
+    #success_url=reverse_lazy(namespace)
+
+class ListarAcudiente(ListView):
+    """
+    Clase encargada de listar los acudientes de la base de datos
+    """
+    #se ingresa qué modelo se utilizará para listar los acudientes
+    model = Acudiente
+    #se indica que template va a gestionar el listador de los acudientes
+    #template_name =
+    #se indica cual será la url de finalización
+    #success_url=reverse_lazy(namespace)
