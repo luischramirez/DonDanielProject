20d43004d67218e9b16ee2e815b82cfec9f7bb0b
= <=>
2017-05-03 23:03:43 -0500

    intentando poner a funcionar pruebas unitarias

--- ProyectoDHS/apps/acudiente/tests.py
+++ ProyectoDHS/apps/acudiente/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+#pylint:disable=E0401
+from apps.acudiente.models import Raza
 
 # Create your tests here.
+class AcudienteTestCase(TestCase):
+    """
+    Clase encargada de probar las funcionalidades de: registro, actualización, borrado y consulta de Acudientes
+    """
+    def setUp(self):
+        """
+        Función encargada de setear los variables necesarios para realizar las pruebas
+        """
+        #Acudiente.objects.create(nombre='Fernando')
+
+
+    def test_verificar_registro_acudiente(self):
+        """
+        Función encargada de verificar si se registró correctamente el acudiente
+        """
+        acudiente1 = Raza.objects.get(id=1)
+        self.assertTrue(acudiente1) 
