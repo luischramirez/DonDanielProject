67109475a7d354ac0d550c4d9a6403d894174254
= <=>
2017-05-08 12:22:35 -0500

    Mejorando calidad codigo notificaciones

--- ProyectoDHS/apps/main/views.py
+++ ProyectoDHS/apps/main/views.py
@@ -4,8 +4,7 @@ from apps.perro.models import Perro, Acudiente
 def index(request):
     perro = Perro.objects.all()
     #acudiente = Acudiente.objects.raw('SELECT id , nombre FROM "Acudiente"')
-    contexto ={ "perros" : perro,
-    }
+    contexto = {"perros" : perro}
     #renderizo o muestro la plantilla de mascotas que tengo
     return render(request, 'main/index.html', contexto)
 
