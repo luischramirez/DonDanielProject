778f22dae26cc913f0b88d958fb8dc3a19ec1238
luischramirez <lchajo115@gmail.com>
2018-03-04 20:36:39 -0500

    Se anexa archivo python con funcion para enviar correo con archivo adjunto y cambios en el archivo backup.bat

--- ProyectoDHS/ProyectoDHS/backup.bat
+++ ProyectoDHS/ProyectoDHS/backup.bat
@@ -1,8 +1,22 @@
 @echo off
-   set BACKUP_FILE=BackupDHS.backup
-   echo backup file name is %BACKUP_FILE%
+for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
+     set dow=%%i
+     set month=%%j
+     set day=%%k
+     set year=%%l
+   )
+   set datestr=%month%_%day%_%year%
+   echo datestr is %datestr%
+
+   set BACKUP_FILE1=BackupDHS.backup
+   set BACKUP_FILE2=BackupDHS_%datestr%.backup
+   echo backup file name is %BACKUP_FILE1%
+   echo backup file name is %BACKUP_FILE2%
    SET PGPASSWORD=Pa$$w0rd
    echo on
-   "C:\Program Files\PostgreSQL\9.4\bin\pg_dump.exe" -h localhost -p 5432 -U postgres -F c -b -v -f "C:\SW3\%BACKUP_FILE%" ProyectoSw3
-
+   "C:\Program Files\PostgreSQL\9.4\bin\pg_dump.exe" -h localhost -p 5432 -U postgres -F c -b -v -f "C:\SW3\%BACKUP_FILE1%" ProyectoSw3
+   if not exist "C:\SW3\SBD\%BACKUP_FILE2%" "C:\Program Files\PostgreSQL\9.4\bin\pg_dump.exe" -h localhost -p 5432 -U postgres -F c -b -v -f "C:\SW3\SBD\%BACKUP_FILE2%" ProyectoSw3
    
+   echo Enviar e-mail
+
+   "C:\SW3\DonDanielProject\ProyectoDHS\ProyectoDHS\send-backup.py"
---
+++ ProyectoDHS/ProyectoDHS/send-backup.py
@@ -0,0 +1,54 @@
+import smtplib, getpass, os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.encoders import encode_base64
+
+print("**** Enviar email con Gmail ***")
+
+user = input("Cuenta de gmail:")
+password = getpass.getpass("Password: ")
+"""
+user = "@gmail.com"
+password = ""
+"""
+
+#Para la cabecera del email
+remitente = "Admin <lchajo115@gmail.com>"
+destinatario = "Luis <ldchavezr@uqvirtual.edu.co>"
+asunto = "Envío de Backup de la aplicación DHS"
+mensaje = "<h1> Este correo contiene el Backup generado por la aplicación del Centro Canino de Servicios Integrales DHS</h1> "
+archivo = "C:\SW3\BackupDHS.backup"
+
+#Host y puerto SMTP de gmail
+gmail = smtplib.SMTP('smtp.gmail.com',587)
+
+#protocolo de cifrado de datos utilizado por gmail
+gmail.starttls()
+
+#credenciales
+gmail.login(user,password)
+
+#muestra la depuracion de la operacion de envio 1=true
+gmail.set_debuglevel(1)
+
+header = MIMEMultipart()
+header['Subject'] = asunto
+header['From'] = remitente
+header['To'] = destinatario
+
+mensaje = MIMEText(mensaje, 'html') #Content-type:text/html
+header.attach(mensaje)
+
+if (os.path.isfile(archivo)):
+    adjunto = MIMEBase('application', 'octet-stream')
+    adjunto.set_payload(open(archivo, "rb").read())
+    encode_base64(adjunto)
+    adjunto.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(archivo))
+    header.attach(adjunto)
+
+#Enviar email
+gmail.sendmail(remitente, destinatario, header.as_string())
+
+#Cerrar la conexion SMTP
+gmail.quit()
